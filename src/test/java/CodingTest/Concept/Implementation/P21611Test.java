b311eef2063038795ea308d6130e2201dabc212a
Juwoong <wwong7247@gmail.com>
2022-03-16 10:57:59 +0900

    [feat] 백준 21611 마법사 상어와 블리자드

--- src/test/java/CodingTest/Concept/Implementation/P21611Test.java
+++ src/test/java/CodingTest/Concept/Implementation/P21611Test.java
@@ -17,13 +17,6 @@ class P21611Test {
         for(int i=0; i<5; i++) list.add(10);
         for(int i=0; i<3; i++) list.add(i);
 
-
-
-
-
-
-
-
 /*  일반식 사용
         int prev = Integer.MIN_VALUE;
         int prevCnt=0;
@@ -57,4 +50,51 @@ class P21611Test {
 
     }
 
+
+    @Test
+    public void simpleTest(){
+
+        String str = "taaaaaaad";
+        int idx = str.indexOf('b');
+        System.out.println(idx);
+
+        //내가 궁금한것
+        str = str.substring(8);
+        System.out.println(str);
+    }
+
+    @Test
+    public void justTest(){
+        String a = "abc";
+        System.out.println(1 + 2+ a);
+        System.out.println(1 + a+ 2);
+        System.out.println(1 +2 +a+ 1+ 2);
+        System.out.println(a+ 2+1);
+
+        System.out.println(1 + 2);
+        System.out.println('a' + 'b');
+        System.out.println("1" + "2");
+    }
+
+    @Test
+    public void ttt(){
+        int a =  7;
+        int b =  3;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println(Integer.toBinaryString(a&b));
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
